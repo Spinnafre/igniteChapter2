820ed8d247cd9c795e9fcd207fd9678578aca8b3
The_King_Davis <61525268+Spinnafre@users.noreply.github.com>
2021-10-02 11:38:49 -0300

    Production confi

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  //Predefinições do babel-transpiler (Compilador do Babel), indicando o modo
+  //específico do transpilador.
     presets: [
+      //Com o babel-preset-env você especifica o ambiente que o código irá ser compilado, no caso para a versão mais recente do node.
       ["@babel/preset-env", { targets: { node: "current" } }],
       "@babel/preset-typescript",
     ],
+    // Plugins usados no transpile, se deseja usar novas features tem que instalar o pacote npm e adicionar aqui
     plugins: [
       [
         "module-resolver",
