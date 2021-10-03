096f66474cf45ba01e22a136d04fd76d54092836
The_King_Davis <61525268+Spinnafre@users.noreply.github.com>
2021-10-03 09:44:23 -0300

    Gerar build e rodar server.js

--- babel.config.js
+++ babel.config.js
@@ -3,25 +3,15 @@ module.exports = {
   //específico do transpilador.
     presets: [
       //Com o babel-preset-env você especifica o ambiente que o código irá ser compilado, no caso para a versão mais recente do node.
-      ["@babel/preset-env", { targets: { node: "current" } }],
+      ["@babel/preset-env", { targets: { node: true},"modules": "auto" }],
       "@babel/preset-typescript",
     ],
     // Plugins usados no transpile, se deseja usar novas features tem que instalar o pacote npm e adicionar aqui
     plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@modules": "./src/modules",
-            "@config": "./src/config",
-            "@shared": "./src/shared",
-            "@errors": "./src/errors",
-            "@utils": "./src/utils",
-          },
-        },
-      ],
       "babel-plugin-transform-typescript-metadata",
       ["@babel/plugin-proposal-decorators", { legacy: true }],
       ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
     ],
   };
